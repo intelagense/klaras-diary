b292c8aea56844c1160bf1a1579284945a98ddc7
intelagense <intelagense@gmail.com>
2023-05-05 18:46:59 -0500

    add gamedata object, ui skeleton, darkmode toggle

---
+++ public/js/gamedata.js
@@ -0,0 +1,100 @@
+const diary = [
+    {
+        day: 1,
+        phrase: "redacted",
+        metadata: "redacted",
+        title: "redacted",
+        nightmare: "redacted",
+        nightmareFlags: ["Initiare"],
+        journal: "redacted",
+        journalFlags: ["Klara", "Selt"]
+    },
+    {
+        day: 2,
+        phrase: "redacted",
+        metadata: "redacted",
+        title: "redacted",
+        nightmare: "redacted",
+        nightmareFlags: ["Shift"],
+        journal: "redacted",
+        journalFlags: ["Karm", "Selt", "redacted"]
+    },
+    {
+        day: 3,
+        phrase: "redacted",
+        metadata: "redacted",
+        title: "redacted",
+        nightmare: "redacted",
+        nightmareFlags: ["Gate"],
+        journal: "redacted",
+        journalFlags: ["redacted", "Karm"]
+    }
+]
+
+const Flags = [
+    {
+        ident: "Selt",
+        datatype: "T-W-X",
+        report: "redacted"
+    },
+    {
+        ident: "Klara",
+        datatype: "U-Y-V",
+        report: "redacted"
+    },
+    {
+        ident: "Karm",
+        datatype: "C-A-X",
+        report: "redacted"
+    },
+    {
+        ident: "Lash",
+        datatype: "MOH",
+        report: "redacted"
+    },
+    {
+        ident: "Initiatre",
+        datatype: "MOH",
+        report: "redacted"
+    },
+    {
+        ident: "redacted",
+        datatype: "U-A-V",
+        report: "redacted"
+    },
+    {
+        ident: "Chronos",
+        datatype: "resource",
+        report: "redacted"
+    },
+    {
+        ident: "A.L.E.S.",
+        datatype: "",
+        report: "redacted"
+    },
+    {
+        ident: "CORTEX",
+        datatype: "",
+        report: "redacted"
+    },
+    {
+        ident: "E.D.P.",
+        datatype: "Ethical Deployment Program",
+        report: "Ethical Deployment Programs or E.D.P.s are dual purpose service machines. They are outfitted for mining and excavation work as well as search and rescue. Additionally they are used as security devices in redacted"
+    },
+    {
+        ident: "",
+        datatype: "",
+        report: "redacted"
+    },
+    {
+        ident: "",
+        datatype: "",
+        report: "redacted"
+    },
+    {
+        ident: "",
+        datatype: "",
+        report: "redacted"
+    },
+]
--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,22 @@
-// let alice = `Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice “without pictures or conversations?”
+const alice = SeededShuffle.shuffle(`“and what is the use of a book,” thought Alice “without pictures or conversations?”`.split(' '), 4)
+const slider = document.getElementById('myRange');
 
-// So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.
+const themeToggle = document.querySelector(".theme-toggle")
 
-// There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, “Oh dear! Oh dear! I shall be late!” (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.`
+themeToggle.addEventListener("click", toggleIt)
 
-let alice = `“and what is the use of a book,” thought Alice “without pictures or conversations?”`
-
-// “and the is thought use of a conversations?” what Alice “without pictures or book,”
-
-// “and what is the conversations?” of a book,” thought Alice “without pictures or use
-
-const displayText = document.querySelector("#text")
-
-function scramble(array) {
-
-    for (var i = array.length - 1; i > 0; i -= 1000) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+function toggleIt() {
+    if (document.documentElement.dataset.theme === "dark") {
+        document.documentElement.dataset.theme = "light"
+        themeToggle.classList.remove = "theme-toggle--toggled" //fix this
+    } else {
+        document.documentElement.dataset.theme = "dark"
+        themeToggle.classList.add = "theme-toggle--toggled" //fix this
     }
-
-    displayText.innerText = array.join(' ')
 }
 
-scramble(alice.split(' '))
+document.querySelector('#text').innerText = SeededShuffle.unshuffle(alice, 1, true).join(' ')
+
+slider.oninput = function () {
+    document.querySelector('#text').innerText = SeededShuffle.unshuffle(alice, this.value, true).join(' ')
+}
